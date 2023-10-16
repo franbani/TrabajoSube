b1cac5f1f082a5531f38c02509915c415c2e3145
franbani <bani.francoo@gmail.com>
2023-10-16 11:30:10 -0300

    Saldo de la tarjeta.

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -12,6 +12,20 @@ class Colectivo{
         $this->linea = $linea;
     }
 
+    private function acreditarSaldoPend($tarj){
+        if($tarj->saldoPendiente > 0){
+            if($tarj->saldo + $tarj->saldoPendiente > $tarj->saldoMax){
+                $acreditando = $tarj->saldoMax - $tarj->saldo;
+                $tarj->saldoPendiente -= $acreditando;
+            }
+            else{
+                $acreditando = $tarj->saldoPendiente;
+                $tarj->saldoPendiente = 0;
+            }
+            $tarj->saldo += $acreditando;
+        }
+    }
+
 
     // pagarCon toma un objeto de clase Tarjeta, descuenta el precio del pasaje al saldo, crea un objeto de clase Boleto y retorna el resultado de generarBoleto o de saldoIns
     public function pagarCon($tarjeta){
@@ -26,11 +40,12 @@ class Colectivo{
                 $tarjeta->viajesHoy += 1;
                 return $boleto->generarBoleto($tarjeta);
             }
-    
-            else if($tarjeta->tipo == "parcial" && $tarjeta->viajesHoy < 4){
+            
+            else if ($tarjeta->tipo == "parcial" && $tarjeta->viajesHoy < 4){
                 if (($tarjeta->saldo - ($this->costePasaje / 2 )) >= $this->saldoMin){
                     $tarjeta->viajesHoy += 1;
-                    $tarjeta->saldo = $tarjeta->saldo - ($this->costePasaje / 2);
+                    $tarjeta->saldo -= ($this->costePasaje / 2);
+                    $this->acreditarSaldoPend($tarjeta);
                     return $boleto->generarBoleto($tarjeta);
                 }
                 else {
@@ -40,7 +55,8 @@ class Colectivo{
     
             else{
                 if (($tarjeta->saldo - $this->costePasaje) >= $this->saldoMin){
-                    $tarjeta->saldo = $tarjeta->saldo - $this->costePasaje;
+                    $tarjeta->saldo -= $this->costePasaje;
+                    $this->acreditarSaldoPend($tarjeta);
                     $tarjeta->viajesHoy += 1;
                     return $boleto->generarBoleto($tarjeta);
                 }
--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -5,29 +5,31 @@ namespace TrabajoSube;
 class Tarjeta{
 
     public $tipo = "comun";
-    public $fyhUltPago = 0;
-    public $viajesHoy = 0;
+    public $saldoPendiente = 0;
 
     public function __construct($sald = 0, $id = 1){ // $saldo e id son inherentes a cada tarjeta
         $this->saldo = $sald;
         $this->id = $id;
     }
 
-    private $saldoMax = 6600;
+    public $saldoMax = 6600;
 
     public function cargaTarjeta($tarjeta, $carga){ // Toma un objeto de clase Tarjeta y un entero, y si la carga no excede el saldoMax y el valor de carga es valido, se acredita la carga y se retorna el nuevo saldo
 
         if(($tarjeta->saldo + $carga) > $this->saldoMax){
-            $texto = "Te pasaste del saldo maximo (6600)";
+            $this->saldoPendiente = $tarjeta->saldo + $carga - $this ->saldoMax;
+            $tarjeta->saldo = 6600;
+            $texto = "Te pasaste del saldo maximo ($6600). Se cargará la tarjeta hasta este saldo y el excedente se acreditará a medida que se use la tarjeta.";
             return $texto;
         }
         else if(!(($carga >= 150 and $carga <= 500 and ($carga % 50) == 0) or ($carga >= 600 and $carga <= 1500 and ($carga % 100) == 0) or ($carga >= 2000 and $carga <= 4000 and ($carga % 500) == 0))){
             $texto = 'Valor de carga invalido. Los valores validos son: 150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500 o 4000';
             return $texto;
         }
-        else{
+        else if (($tarjeta->saldo + $carga) <= $this->saldoMax){
             $tarjeta->saldo = $tarjeta->saldo + $carga;
-            return $tarjeta->saldo;
+            $texto = 'Se han cargado $' . $carga . '. Saldo final: $' . $tarjeta->saldo;
+            return $texto;
         }
     }
 
