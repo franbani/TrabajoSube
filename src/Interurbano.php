e6ab14a6e435cf64f274c7221ca74e6c0d928db8
franbani <bani.francoo@gmail.com>
2023-10-16 11:38:58 -0300

    Lineas I

--- src/Interurbano.php
+++ src/Interurbano.php
@@ -4,7 +4,7 @@ namespace TrabajoSube;
 
 class Interurbano extends Colectivo{
     private $saldoMin = -211.84;
-    public $costePasaje = 270;
+    public $costePasaje = 184;
 
     public function __construct($linea = "Expreso"){
         $this->linea = $linea;
